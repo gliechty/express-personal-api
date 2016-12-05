2b380aaa129efbaa32b380dc6e6c0300fc8e3b9a
gliechty <guy.liechty@gmail.com>
2016-12-05 13:32:28 -0700

    trying to push changes to heroku

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
  * DATABASE *
  ************/
 
-// var db = require('./models');
+var db = require('./models');
 
 /**********
  * ROUTES *
@@ -31,6 +31,8 @@ app.get('/', function homepage(req, res) {
 });
 
 
+
+
 /*
  * JSON API Endpoints
  */
@@ -38,17 +40,23 @@ app.get('/', function homepage(req, res) {
 app.get('/api', function api_index(req, res) {
   // TODO: Document all your api endpoints below
   res.json({
-    woops_i_has_forgot_to_document_all_my_endpoints: true, // CHANGE ME ;)
+    // woops_i_has_forgot_to_document_all_my_endpoints: true, // CHANGE ME ;)
     message: "Welcome to my personal api! Here's what you need to know!",
-    documentation_url: "https://github.com/example-username/express_self_api/README.md", // CHANGE ME
-    base_url: "http://YOUR-APP-NAME.herokuapp.com", // CHANGE ME
+    documentation_url: "https://github.com/gliechty/express-personal-api/README.md", // CHANGE ME
+    base_url: "https://desolate-dawn-69192.herokuapp.com/", 
     endpoints: [
       {method: "GET", path: "/api", description: "Describes all available endpoints"},
       {method: "GET", path: "/api/profile", description: "Data about me"}, // CHANGE ME
       {method: "POST", path: "/api/campsites", description: "E.g. Create a new campsite"} // CHANGE ME
     ]
-  })
+  });
 });
+
+// my code//
+
+// app.get('/api/', );
+
+// end my code//
 
 /**********
  * SERVER *
