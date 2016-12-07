aedf4bbf9040160b09893f60befbc09eae71fd62
gliechty <guy.liechty@gmail.com>
2016-12-07 16:26:45 -0700

    seed file deletes old artists and adds three

--- seed.js
+++ seed.js
@@ -3,14 +3,42 @@
 
 var db = require('./models');
 
-var new_artist = {name: "Bob Dylan", genre: "Rock"};
 
-db.Artist.create(new_artist, function(err, artist){
-  if (err){
-    return console.log("Error:", err);
-  }
-
-  console.log("Created new artist", artist._id);
-  process.exit(); // we're all done! Exit the program.
+var artistList = [];
+artistList.push({
+	name: "Bob Dylan", 
+	genre: "Blues/Rock"
+	});
+artistList.push({
+	name: "Seal",
+	genre: "Pop?"
+	});
+artistList.push({
+	name: "Isaac Hayes",
+	gernre: "Soul"
 });
+
+db.Artist.remove({}, function(err, artists){
+
+	db.Artist.create(artistList, function (err, album){
+		if (err) {return console.log('Err: ', err);}
+		console.log("artists added", artists);
+		process.exit();
+	});
+});
+
+
+
+// app.get('/api/artists', function artist_index(req, res){
+// 	console.log("seeding json");
+// 	res.json({})
+// })
+// 	db.Artist.create(new_artist, function(err, artist){
+//   	if (err){
+//     return console.log("Error:", err);
+//   }
+
+//   console.log("Created new artist", artist._id);
+//   process.exit(); // we're all done! Exit the program.
+// });
 
